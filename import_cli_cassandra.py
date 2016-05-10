27f61904e109efa0d062dac22828bba5be50df0e
zyblzb <eanbo@lzb.ac.cn>
2016-05-10 21:36:27 +0800

    取消时间解析

--- import_cli_cassandra.py
+++ import_cli_cassandra.py
@@ -6,22 +6,23 @@ from cassandra.cluster import Cluster
 
 cluster = Cluster(['192.168.1.66'])
 session = cluster.connect('clichn') ## USE mykeyspace;
-path = u'D:/data/data.cma_surf_cli.日值地面气象/data'
+#path = 'D:/data/test'
+path = u'D:/data/tem'
 filelist = glob.glob(os.path.join(path,'*.txt'))
-cqlstr = 'INSERT INTO tem (station, date, longitude, latitude, elevation, year, month, day, tem_avg, tem_max, tem_min, qc_avg, qc_max, qc_min) \
+cqlstr = 'INSERT INTO clichn.tem (station, date, longitude, latitude, elevation, year, month, day, tem_avg, tem_max, tem_min, qc_avg, qc_max, qc_min) \
 VALUES( %s,"%s",%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s );'
 cqlstr = '''
-INSERT INTO tem (station, date, longitude, latitude, elevation, year, month, day, tem_avg, tem_max, tem_min, qc_avg, qc_max, qc_min)
+INSERT INTO clichn.tem (station, date, longitude, latitude, elevation, year, month, day, tem_avg, tem_max, tem_min, qc_avg, qc_max, qc_min)
 VALUES( %s,'%s',%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s );
 '''
 query  = '''
-    INSERT INTO tem (station, date, longitude, latitude, elevation, year, month, day, tem_avg, tem_max, tem_min, qc_avg, qc_max, qc_min)
+    INSERT INTO clichn.tem (station,date,longitude, latitude, elevation, year, month, day, tem_avg, tem_max, tem_min, qc_avg, qc_max, qc_min)
 VALUES( ?,?,?,?,?,?,?,?,?,?,?,?,?,? );
 '''
 prepared = session.prepare(query)
 
 
-batch_size = 200 #1000 too large
+batch_size = 200 #200,    (1000 too large)
 insert_stmt = query
 
 i=0
@@ -34,24 +35,28 @@ for filename in filelist:
             col_widths = [5,5,6,7,5,3,3,7,7,7,2,2,2]
 
             print '----', filename, '----'
-            dataset = pd.read_fwf(filename,widths=col_widths,\
-                           names=cols,\
-                           parse_dates={'date':['year','month','day']})
+            dataset = pd.read_fwf(filename,widths=col_widths, names=cols)#parse_dates={'date':['year','month','day']}
                            
-            batch_stmt = 'BEGIN BATCH '
+            batch_stmt = 'BEGIN UNLOGGED BATCH '
             for id in dataset.index:
                 ### BATCH
-                batch_stmt += cqlstr %(dataset.ix[id,'station'], dataset.ix[id,'date'], dataset.ix[id,'longitude'], dataset.ix[id,'latitude'], \
-                dataset.ix[id,'elevation'], dataset.ix[id,'date'].year, dataset.ix[id,'date'].month, dataset.ix[id,'date'].day, \
+                date = '%s-%s-%s 00:00:00-0800'%(dataset.ix[id,'year'], dataset.ix[id,'month'], dataset.ix[id,'day'])
+                batch_stmt += cqlstr %(dataset.ix[id,'station'], date, dataset.ix[id,'longitude'], dataset.ix[id,'latitude'], \
+                dataset.ix[id,'elevation'], dataset.ix[id,'year'], dataset.ix[id,'month'], dataset.ix[id,'day'], \
                 dataset.ix[id,'tem_avg'], dataset.ix[id,'tem_max'], dataset.ix[id,'tem_min'], \
                 dataset.ix[id,'qc_avg'], dataset.ix[id,'qc_max'], dataset.ix[id,'qc_min'] ) 
-                
+                '''print '--', dataset.ix[id,'station'], dataset.ix[id,'date'], dataset.ix[id,'longitude'], dataset.ix[id,'latitude'], \
+                dataset.ix[id,'elevation'], dataset.ix[id,'date'].year, dataset.ix[id,'date'].month, dataset.ix[id,'date'].day, \
+                dataset.ix[id,'tem_avg'], dataset.ix[id,'tem_max'], dataset.ix[id,'tem_min'], \
+                dataset.ix[id,'qc_avg'], dataset.ix[id,'qc_max'], dataset.ix[id,'qc_min']   #'''
                 i+=1
-                if( id%batch_size==0 or id==(len(dataset.index)-1)):
+                if( (id+1)%batch_size==0 or id==(len(dataset.index)-1)):
                     batch_stmt += 'APPLY BATCH;'
+                    ###print '--------------------'
+                    ###print '%d    ', batch_stmt
                     pre_batch = session.execute(batch_stmt)  #session.prepare(batch_stmt)
                     batch_stmt = 'BEGIN BATCH '
-                    print '%d    '%(i), dataset.ix[id,'station'], dataset.ix[id,'date']
+                    print '%d    '%(i), dataset.ix[id,'station'], date
                 ###=================================
                 '''
                 ### prepared statements
